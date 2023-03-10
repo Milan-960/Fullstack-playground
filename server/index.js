f90ee802c13817d6f3ce04ff2db80256bd52ffb8
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-10 20:23:42 +0100

    read me updated

--- server/index.js
+++ server/index.js
@@ -46,12 +46,27 @@
 // });
 
 import express from "express";
-// import path from "path";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+
 import data from "./src/data.js";
 import img from "./src/img.js";
 
 const app = express();
+app.use(bodyParser.json()); // to use body object in requests
 const PORT = process.env.PORT || 2002;
+dotenv.config();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+// CDN CSS
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 // This code adds the necessary CORS headers to allow any domain to make requests to your server.
 app.use((req, res, next) => {
@@ -63,13 +78,48 @@ app.use((req, res, next) => {
   next();
 });
 
-// Route for the root path
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:2002",
+        description: "My API Documentation",
+      },
+    ],
+  },
+  // This is to call all the file
+  apis: ["src/**/*.js"],
+};
+
+// Setting up the swagger api docs
+const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
+
+//* Route for the root path
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 // Route for the data
-app.get("/test", (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(data);
 });
 
