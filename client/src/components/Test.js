a11b6cf7790072417862671a215113d74bb5df25
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-10 19:50:33 +0100

    api corrected

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -14,14 +14,16 @@ export const Test = () => {
   return (
     <div>
       <h1>This is node</h1>
-      {data.map((item, index) => {
-        return (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <img src={item.img} alt="fake" />
-          </div>
-        );
-      })}
+      <div className="img-container">
+        {data.map((item, index) => {
+          return (
+            <div key={index} className="img-map">
+              <h2>{item.title}</h2>
+              <img src={item.img} alt="fake" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
