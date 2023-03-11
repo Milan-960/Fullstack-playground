8e7871f6f4d8aa8aa40fd87f6f14c1935a6dc2a0
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-11 18:44:44 +0100

    read me updated

--- server/src/data.js
+++ server/src/data.js
@@ -110,75 +110,121 @@
 import fs from "fs";
 import path from "path";
 
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-
-// Import translation files
-import EN from "./translations/en.json" assert { type: "json" };
-import FR from "./translations/fr.json" assert { type: "json" };
-
-// Configure i18n module to use translation files from 'translation' folder
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: EN,
-    },
-    fr: {
-      translation: FR,
-    },
-  },
-  lng: "en",
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false, // not needed for react as it escapes by default
-  },
-  react: {
-    useSuspense: false,
-  },
-});
-
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-const images = fs
-  .readdirSync(path.join(__dirname, "../public/images"))
-  .filter((filename) => filename.endsWith(".png"))
-  .map((filename) => {
-    const data = fs.readFileSync(
-      path.join(__dirname, "../public/images", filename),
-      {
+const imagesDirPath = path.join(__dirname, "../../../public/images");
+let images = [];
+if (fs.existsSync(imagesDirPath)) {
+  images = fs
+    .readdirSync(imagesDirPath)
+    .filter((filename) => filename.endsWith(".png"))
+    .map((filename) => {
+      const data = fs.readFileSync(path.join(imagesDirPath, filename), {
         encoding: "base64",
-      }
-    );
-    return `data:image/png;base64,${data}`;
-  });
-
-const title = i18n.t("hello");
+      });
+      return `data:image/png;base64,${data}`;
+    });
+}
 
 const data = [
   {
     userId: 1,
     id: 1,
-    title,
-    body: "this is test",
+    title: "Lorem ipsum dolor sit amet",
+    body: "this is test1",
     img: images[0],
   },
   {
     userId: 2,
     id: 2,
-    title: i18n.t("Hello World!"),
+    title: "Lorem ipsum dolor sit amet",
     body: "this is test",
     img: images[1],
   },
   {
     userId: 3,
     id: 3,
-    title: i18n.t("Hello World!"),
+    title: "Lorem ipsum dolor sit amet",
     body: "this is test",
     img: images[2],
   },
 ];
 
 export default data;
+
+// This is for I18n testing
+// import fs from "fs";
+// import path from "path";
+
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+
+// // Import translation files
+// import EN from "./translations/en.json" assert { type: "json" };
+// import FR from "./translations/fr.json" assert { type: "json" };
+
+// // Configure i18n module to use translation files from 'translation' folder
+// i18n.use(initReactI18next).init({
+//   resources: {
+//     en: {
+//       translation: EN,
+//     },
+//     fr: {
+//       translation: FR,
+//     },
+//   },
+//   lng: "en",
+//   fallbackLng: "en",
+//   interpolation: {
+//     escapeValue: false, // not needed for react as it escapes by default
+//   },
+//   react: {
+//     useSuspense: false,
+//   },
+// });
+
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// const images = fs
+//   .readdirSync(path.join(__dirname, "../public/images"))
+//   .filter((filename) => filename.endsWith(".png"))
+//   .map((filename) => {
+//     const data = fs.readFileSync(
+//       path.join(__dirname, "../public/images", filename),
+//       {
+//         encoding: "base64",
+//       }
+//     );
+//     return `data:image/png;base64,${data}`;
+//   });
+
+// const title = i18n.t("hello");
+
+// const data = [
+//   {
+//     userId: 1,
+//     id: 1,
+//     title,
+//     body: "this is test",
+//     img: images[0],
+//   },
+//   {
+//     userId: 2,
+//     id: 2,
+//     title: i18n.t("Hello World!"),
+//     body: "this is test",
+//     img: images[1],
+//   },
+//   {
+//     userId: 3,
+//     id: 3,
+//     title: i18n.t("Hello World!"),
+//     body: "this is test",
+//     img: images[2],
+//   },
+// ];
+
+// export default data;
 
 // import fs from "fs";
 // import path from "path";
