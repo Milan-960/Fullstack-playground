120cf6f46525e27f81f1713bd503d3bd9dac068c
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-11 17:27:52 +0100

    i18n added to client and nodejs server woow

--- client/src/i18n/LanguageSwitcher.jsx
+++ client/src/i18n/LanguageSwitcher.jsx
@@ -42,20 +42,47 @@
 // export default LanguageSwitcher;
 
 import React, { useState } from "react";
+import Select from "react-select";
+
 import { useTranslation } from "react-i18next";
-import { switchLanguage } from "./i18n";
+import { switchLanguage, i18n } from "./i18n";
 
 function LanguageSwitcher() {
   const { t } = useTranslation();
-  const [currentLanguage, setCurrentLanguage] = useState("en");
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+
+  const langOptions = [
+    { value: "en", label: "EN" },
+    { value: "fr", label: "FR" },
+  ];
 
   const handleLanguageChange = async (language) => {
     await switchLanguage(language);
     setCurrentLanguage(language);
   };
 
+  const handleLanguageChangeSelect = async (event) => {
+    const lang = event.target.value;
+    await switchLanguage(lang);
+    i18n.changeLanguage(lang);
+    setCurrentLanguage(lang);
+  };
+
   return (
     <div>
+      <Select
+        options={langOptions}
+        value={{ value: i18n.language, label: i18n.language.toUpperCase() }}
+        onChange={(option) => handleLanguageChange(option.value)}
+      />
+      <br></br>
+      <select onChange={handleLanguageChangeSelect} value={i18n.language}>
+        <option value="en">English</option>
+        <option value="fr">Français</option>
+      </select>
+      <br></br>
+      <br></br>
+      &nbsp;
       <button onClick={() => handleLanguageChange("en")}>{t("English")}</button>
       <button onClick={() => handleLanguageChange("fr")}>{t("French")}</button>
       <p>
